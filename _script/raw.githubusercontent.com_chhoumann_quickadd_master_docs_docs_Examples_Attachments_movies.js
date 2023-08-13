ff4ed627b67c1d7b548cd27e3b8b7e7bdcbefe22
mnw1020 <mnw1020@gmail.com>
2023-08-13 14:17:44 +0500

    vault backup: 2023-08-13 14:17:44

--- _script/raw.githubusercontent.com_chhoumann_quickadd_master_docs_docs_Examples_Attachments_movies.js
+++ _script/raw.githubusercontent.com_chhoumann_quickadd_master_docs_docs_Examples_Attachments_movies.js
@@ -1,7 +1,7 @@
 const notice = msg => new Notice(msg, 5000);
 const log = msg => console.log(msg);
 
-const API_KEY_OPTION = "ss;
+const API_KEY_OPTION = "b159eeb1";
 const API_URL = "https://www.omdbapi.com/";
 
 module.exports = {
@@ -13,7 +13,7 @@ module.exports = {
             [API_KEY_OPTION]: {
                 type: "text",
                 defaultValue: "",
-                placeholder: "OMDb API Key",
+                placeholder: "b159eeb1",
             },
         }
     }
